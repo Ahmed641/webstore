33823ed81145a582892562e34b6c6a33f1697b95
Ahmed Mohamed <ahmedm@Ahmeds-MacBook-Pro.local>
2019-12-05 06:25:19 -0500

    commit 3(java)

--- js/index.js
+++ js/index.js
@@ -7,6 +7,80 @@ const products = [
         imgA: 'img/pic2.jpeg',
         imgB: 'img/pic1.jpeg',
         imgC: 'img/pic3.jpeg',
+        price: 34.99,
+        details:'Sweatshirt with long raglan sleeves and ribbing at neckline, cuffs, and hem. Soft, brushed inside. Regular fit.',
+    },
+    {
+        name: '124',
+        imgA: 'img/pic4.jpeg',
+        imgB: 'img/pic5.jpeg',
+        imgC: 'img/pic6.jpeg',
+        price: 64.99,
+        details:'Lightweight, oversized sweatshirt with embroidery. Heavily dropped shoulders and ribbing at neckline, cuffs, and hem.',
+    },
+    {
+        name: '125',
+        imgA: 'img/pic7.jpeg',
+        imgB: 'img/pic8.jpeg',
+        imgC: 'img/pic9.jpeg',
+        price: 99.99,
+        details:'Short, fitted dress in crêped jersey with long puff sleeves. Concealed zip at back. Unlined.',
+    },
+    {
+        name: '126',
+        imgA: 'img/pic10.jpeg',
+        imgB: 'img/pic11.jpeg',
+        imgC: 'img/pic12.jpeg',
+        price:79.99,
+        details:'PREMIUM QUALITY/CONSCIOUS. Short, double-breasted coat in a felted, woven wool blend. Wide, notched lapels and concealed snap fasteners at front. Relaxed fit with dropped shoulders, side-seam pockets, and an inner pocket with a button. Lined. Wool content is recycled.',
+    },
+    {
+        name: '127',
+        imgA: 'img/pic13.jpeg',
+        imgB: 'img/pic14.jpeg',
+        imgC: 'img/pic15.jpeg',
+        price:159.99,
+        details:'Coat in woven, felted wool-blend fabric. Stand-up collar, buttons at front, and decorative buttons at cuffs. Diagonal welt side pockets and two inner pockets. Vent at back. Lined.',
+    },
+    {
+        name: '128',
+        imgA: 'img/pic16.jpeg',
+        imgB: 'img/pic17.jpeg',
+        imgC: 'img/pic18.jpeg',
+        price:29.99,
+        details:'Long-sleeved sweatshirt in soft fabric with a lined drawstring hood, kangaroo pocket, and ribbing at cuffs and hem. Soft, brushed inside.',
+    },
+    {
+        name: '129',
+        imgA: 'img/pic19.jpeg',
+        imgB: 'img/pic20.jpeg',
+        imgC: 'img/pic21.jpeg',
+        price:79.99,
+        details:'Soft sweatshirt with a slightly looser fit. Dropped shoulders and ribbing at neckline, cuffs, and hem. Soft, brushed inside.',
+    },
+    {
+        name: '130',
+        imgA: 'img/pic22.jpeg',
+        imgB: 'img/pic23.jpeg',
+        imgC: 'img/pic24.jpeg',
+        price:29.99,
+        details:'Relaxed-fit sweatshirt with a design. Long sleeves and ribbing at neckline, cuffs, and hem.',
+    },
+    {
+        name: '131',
+        imgA: 'img/pic25.jpeg',
+        imgB: 'img/pic26.jpeg',
+        imgC: 'img/pic27.jpeg',
+        price:79.99,
+        details:'Hooded coat in woven wool-blend fabric. Concealed snap fasteners at front, tie belt at waist, and side pockets. Vent at back. Lined. Wool content is recycled.',
+         },
+    {
+        name: '132',
+        imgA: 'img/pic28.jpeg',
+        imgB: 'img/pic29.jpeg',
+        imgC: 'img/pic30.jpeg',
+        price:129.99,
+        details:  'Straight-cut jacket in woven fabric. Shawl collar, lightweight shoulder pads, welt front pockets, and 3/4-length sleeves with short slit at cuffs. No fasteners. Lined.',
     }
 ]
 
@@ -30,7 +104,7 @@ function getHtmlFromProduct(product) {
         </div>
         </header>
         
-        <p>Sweatshirt with long raglan sleeves and ribbing at neckline, cuffs, and hem. Soft, brushed inside. Regular fit.</p>
+        <p>${product.details}</p>
         <form class="pic_details">
         
             <div class="colours"><legend>Colours</legend>
@@ -57,7 +131,7 @@ function getHtmlFromProduct(product) {
                 <dt>Rating</dt>
                 <dd> <span class="material-icons">star</span><span class="material-icons">star</span><span class="material-icons">star</span><span class="material-icons">star</span><span class="material-icons">star_half</span></dd>
             </dl>
-            <data value="39"><del>$49.99</del> <ins>$34.99</ins></data>
+            <data ${product.price}" value="39"><del>$49.99</del> <ins>$34.99</ins></data>
             <div class="cart"><button class="cart1"> </button>  <span class="material-icons">shopping_basket</span> </button></div>
             <div class="likes"><button class="likes" type="button"><span class="material-icons">favorite_border</span></button></div>
             
@@ -69,9 +143,12 @@ function getHtmlFromProduct(product) {
 
 
 /**************EXUCTION***** */
-itemsInCart = addItemsToTheCart(itemsInCart, 1);//1
-itemsInCart = addItemsToTheCart(itemsInCart, 5);//6
+function showProducts(prodcut){
+    const arrayOdProducts = prodcut.map(getHtmlFromProduct);
+    const strngOfHtml = arrayOdProducts.join('\n');
+    document.getElementById('products').innerHTML = strngOfHtml;
+}
 
 
-/*document.???*/ = products.map(getHtmlFromProduct).join('')
+
 
